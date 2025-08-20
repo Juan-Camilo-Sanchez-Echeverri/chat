e462afccfde8707303403c7512dd8f6da7f73905
Juan Camilo Sanchez <juancamilosanche65@gmail.com>
2025-08-20 16:34:08 -0500

    Refactor UsersService to enhance findActiveInstitutionAdmins method for better role-based user retrieval

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,13 +1,41 @@
 import { Injectable } from '@nestjs/common';
+
 import { InjectConnection } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 
-import { UserDocument } from './types/user.types';
+import { Role, Status } from '@common/enums';
 import { ConnectionService } from '@common/services';
+import { ObjectId } from '@common/types';
+
+import { UserDocument } from './types/user.types';
 
 @Injectable()
 export class UsersService extends ConnectionService<UserDocument> {
   constructor(@InjectConnection() connection: Connection) {
     super(connection, 'users');
+  }
+
+  async findActiveInstitutionAdmins(
+    institution: ObjectId,
+  ): Promise<UserDocument[]> {
+    const [admins, rectors] = await Promise.all([
+      this.collection.find({
+        role: Role.Admin,
+        status: Status.ACTIVE,
+        institution,
+      }),
+      this.collection.find({
+        role: Role.Rector,
+        status: Status.ACTIVE,
+        institution,
+      }),
+    ]);
+
+    const [adminsArray, rectorsArray] = await Promise.all([
+      admins.toArray(),
+      rectors.toArray(),
+    ]);
+
+    return [...adminsArray, ...rectorsArray];
   }
 }
