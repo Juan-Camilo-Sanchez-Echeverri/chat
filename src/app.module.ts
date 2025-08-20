4e3a6b9d9c6331dd491544e1c70c436713bf4fad
Juan Camilo Sanchez <juancamilosanche65@gmail.com>
2025-08-20 16:07:47 -0500

    Refactor app module: reorganize imports and add StudentsModule for better structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common';
 
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { MongooseConfigService } from './config/db';
-import { ChatModule } from './modules/chat/chat.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { UsersModule } from './modules/users/users.module';
-import { ChatMessagesModule } from './modules/chat-messages/chat-messages.module';
+import { MongooseConfigService } from '@config/db';
+
+import { AuthModule } from '@modules/auth/auth.module';
+import { ChatMessagesModule } from '@modules/chat-messages/chat-messages.module';
+import { ChatModule } from '@modules/chat/chat.module';
+import { StudentsModule } from '@modules/students/students.module';
+import { UsersModule } from '@modules/users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    // Global common modules
     MongooseModule.forRootAsync({ useClass: MongooseConfigService }),
+
+    // Business modules
     ChatModule,
     AuthModule,
     UsersModule,
     ChatMessagesModule,
+    StudentsModule,
   ],
 })
 export class AppModule {}
